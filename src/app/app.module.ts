ad0372e0e4334538e35ea141f53eb40787563814
Arsmtrong1995 <seperalta@ucompensar.edu.co>
2023-11-09 03:07:59 -0500

    Ajuster y creacion card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,27 +8,30 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { FajasComponent } from './pages/fajas/fajas.component';
 import { InsumosComponent } from './pages/insumos/insumos.component';
 import { WhatsappBtnComponent } from './components/whatsapp-btn/whatsapp-btn.component';
+import { CardsComponent } from './components/cards/cards.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LandingComponent,
-    ContactComponent,
-    FajasComponent,
-    InsumosComponent,
-    WhatsappBtnComponent,
-    FooterComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LandingComponent,
+        ContactComponent,
+        FajasComponent,
+        InsumosComponent,
+        WhatsappBtnComponent,
+        FooterComponent,
+        HeaderComponent,
+        CardsComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        
+    ]
 })
 export class AppModule { }
